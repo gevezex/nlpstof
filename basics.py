1c420c0f0da612d29d577971a42b67b124296de5
gevezex <gevezex@gmail.com>
2020-02-16 11:59:05 +0100

    Added document2sent2tokens function

--- basics.py
+++ basics.py
@@ -11,7 +11,8 @@ def tokenize_sentences(sentences):
     '''input: list of sentences in strings
        output: tokenized list of tokenized sentences'''
     nlp = spacy.load('nl')
-    list_of_sentence_tokens = [[token.text for token in nlp(sent)] for sent in sentences]
+    #list_of_sentence_tokens = [[token.text for token in nlp(sent)] for sent in sentences]
+    list_of_sentence_tokens = [[token.text for token in doc] for doc in nlp.pipe(sentences)]
     return list_of_sentence_tokens
 
 import nltk 
@@ -21,3 +22,11 @@ def paragraph2sentences(paragraph):
        output: list of sentences in string format'''
     sent_text = nltk.sent_tokenize(paragraph)
     return sent_text
+
+def document2sent2tokens(document):
+    """input: large string document
+    output: list of tokens per sentence """
+    
+    sent_text = paragraph2sentences(document)
+    tokenized_sentences = tokenize_sentences(sent_text)
+    return tokenized_sentences
