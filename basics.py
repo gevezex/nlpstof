b125c05af47fafa772c9fc5edd4b67ba4b47f74a
gevezex <gevezex@gmail.com>
2020-02-16 11:00:53 +0100

    New added funcionts tokenize_sentences

--- basics.py
+++ basics.py
@@ -1,4 +1,15 @@
 import spacy 
 
 def tokenize_sentence(sentence):
-    return
+    '''input: sentence string
+       output: tokenized list of words'''
+    nlp = spacy.load('nl')
+    doc = nlp(sentence)
+    return [token.text for token in doc]
+
+def tokenize_sentences(sentences):
+    '''input: list of sentences in strings
+       output: tokenized list of tokenized sentences'''
+    nlp = spacy.load('nl')
+    list_of_sentence_tokens = [[token.text for token in nlp(sent)] for sent in sentences]
+    return list_of_sentence_tokens
