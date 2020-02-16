646f02c5a96ffd76013a9b4b4723acc081ec3c22
gevezex <gevezex@gmail.com>
2020-02-16 12:34:13 +0100

    Refactoring

--- basics.py
+++ basics.py
@@ -1,16 +1,22 @@
 import spacy 
 
-def tokenize_sentence(sentence):
-    '''input: sentence string
-       output: tokenized list of words'''
-    nlp = spacy.load('nl')
+def tokenize_sentence(sentence, spacy_model="nl"):
+    """
+    input: sentence string
+    output: tokenized list of words'''
+    nlp = spacy.load(spacy_model)
     doc = nlp(sentence)
     return [token.text for token in doc]
+    """
 
-def tokenize_sentences(sentences):
-    '''input: list of sentences in strings
-       output: tokenized list of tokenized sentences'''
-    nlp = spacy.load('nl')
+def tokenize_sentences(sentences, spacy_model="nl"):
+    """
+    input: list of sentences in strings
+    input: spacy_model, default = dutch
+    output: tokenized list of tokenized sentences
+    """
+
+    nlp = spacy.load(spacy_model)
     #list_of_sentence_tokens = [[token.text for token in nlp(sent)] for sent in sentences]
     list_of_sentence_tokens = [[token.text for token in doc] for doc in nlp.pipe(sentences)]
     return list_of_sentence_tokens
@@ -18,15 +24,20 @@ def tokenize_sentences(sentences):
 import nltk 
 
 def document2sentences(document):
-    '''input: large string document
-       output: list of sentences in string format'''
+    """
+    input: large string document
+    output: list of sentences in string format
     sent_text = nltk.sent_tokenize(document)
     return sent_text
+    """
 
-def document2sent2tokens(document):
-    """input: large string document
-    output: list of tokens per sentence """
+def document2sent2tokens(document, spacy_model="nl"):
+    """
+    input: large string document
+    input: spacy_model, default = dutch
+    output: list of tokens per sentence
+    """
     
     sent_text = document2sentences(document)
-    tokenized_sentences = tokenize_sentences(sent_text)
+    tokenized_sentences = tokenize_sentences(sent_text, spacy_model)
     return tokenized_sentences
