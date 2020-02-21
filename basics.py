597e6d67cf834dda64038e9d953e672882eecd6c
gevezex <gevezex@gmail.com>
2020-02-21 18:05:22 +0100

    removed all standalone functions

--- basics.py
+++ basics.py
@@ -9,57 +9,6 @@ from pdfminer.layout import LAParams
 from pdfminer.pdfpage import PDFPage
 
 
-def tokenize_sentence(sentence, spacy_model="nl"):
-    """
-    input: sentence string
-    output: tokenized list of words
-    """
-    nlp = spacy.load(spacy_model)
-    doc = nlp(sentence)
-    return [token.text for token in doc]
-
-def tokenize_sentences(sentences, spacy_model="nl"):
-    """
-    input: list of sentences in strings
-    input: spacy_model, default = dutch
-    output: tokenized list of tokenized sentences
-    """
-
-    nlp = spacy.load(spacy_model)
-    #list_of_sentence_tokens = [[token.text for token in nlp(sent)] for sent in sentences]
-    list_of_sentence_tokens = [[token.text for token in doc] for doc in nlp.pipe(sentences)]
-    return list_of_sentence_tokens
-
-import nltk 
-
-def document2sentences(document, language='dutch', eol=True):
-    """
-    input: large string document
-    output: list of sentences in string format
-    """
-    sent_text = nltk.sent_tokenize(document)
-    if eol:
-        tmp_list = ['Eerste zin']
-        for sentence in sent_text:
-            tmp_list.extend(sentence.split('\n'))
-        del tmp_list[0]
-        sent_text = tmp_list
-
-    return sent_text
-    
-
-def document2sent2tokens(document, spacy_model="nl"):
-    """
-    input: large string document
-    input: spacy_model, default = dutch
-    output: list of tokens per sentence
-    """
-    
-    sent_text = document2sentences(document)
-    tokenized_sentences = tokenize_sentences(sent_text, spacy_model)
-    return tokenized_sentences
-
-
 class documentTokenizer:
     """ Tokenizer for documents """
 
